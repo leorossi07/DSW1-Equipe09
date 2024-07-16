b22bd116d8266ebcb1397fef99a79fe57d1d0997
Léo Rossi <94882833+leorossi07@users.noreply.github.com>
2024-07-16 17:39:46 -0300

    Update CandidaturaController.java

--- Jobfying/src/main/java/br/ufscar/dc/dsw/controller/CandidaturaController.java
+++ Jobfying/src/main/java/br/ufscar/dc/dsw/controller/CandidaturaController.java
@@ -17,6 +17,7 @@ import javax.servlet.http.HttpServletResponse;
 import br.ufscar.dc.dsw.dao.CandidaturaDAO;
 import br.ufscar.dc.dsw.dao.VagaDAO;
 import br.ufscar.dc.dsw.domain.Candidatura;
+import br.ufscar.dc.dsw.domain.Profissional;
 import br.ufscar.dc.dsw.domain.Vaga;
 import br.ufscar.dc.dsw.domain.Usuario;
 
@@ -40,9 +41,87 @@ public class CandidaturaController extends HttpServlet{
         doGet(request, response);
     }
 
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+    	
+    	Usuario usuario = (Usuario) request.getSession().getAttribute("usuarioLogado");
+    	Erro erros = new Erro();
+    	
+    	if(usuario == null) {
+    		response.sendRedirect(request.getContextPath());
+    		return;
+    	}else if(!usuario.getPapel().equals("PROFISSIONAL")) {
+    		erros.add("Acesso não autorizado");
+    		erros.add("Apenas papel [PROFISSIONAL] tem acesso a essa página");
+    		request.setAttribute("mensagens", erros);
+    		RequestDispatcher rd = request.getRequestDispatcher("/noAuth.jsp");
+    		rd.forward(request, response);
+    		return;
+    	}
+    	
+    	
+    	String action = request.getPathInfo();
+    	if(action == null) {
+    		action = "";
+    	}
+    	
+    	try {
+    		switch(action){
+    			case "/cadastro":
+    				apresentaFormCadastro(request, response);
+    				break;
+    			case "/insercao":
+    				insere(request, response);
+    				break;
+    			default:
+    				lista(request, response);
+    				break;
+    		}
+    	} catch(RuntimeException | IOException | ServletException e) {
+    		throw new ServletException(e);
+    	}
+    }
     
+    private void lista(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	Profissional profissional = (Profissional) request.getSession().getAttribute("profissionalLogado");
+    	List<Candidatura> listaCandidaturas = dao.getAllByProfissional(profissional);
+    	request.setAttribute("listaCandidaturas", listaCandidaturas);
+    	RequestDispatcher dispatcher = request.getRequestDispatcher("/logado/candidatura/lista.jsp");
+    	dispatcher.forward(request, response);
+    }
     
+    private Map<Long, Vaga> getVagas(){
+    	Map<Long, Vaga> vagas = new HashMap<>();
+    	for(Vaga vaga: new VagaDAO().getAll()) {
+    		vagas.put(vaga.getId(), vaga);
+    	}
+    	return vagas;
+    }
     
+    private void apresentaFormCadastro(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+    	request.setAttribute("vagas", getVagas());
+    	RequestDispatcher dispatcher = request.getRequestDispatcher("/logado/candidatura/formulario.jsp");
+    	dispatcher.forward(request, response);
+    }
     
-    
+    private void insere(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+    	request.setCharacterEncoding("UTF-8");
+    	
+    	Long id = Long.parseLong(request.getParameter("vaga"));
+    	
+    	Vaga vaga = new VagaDAO().get(id);
+    	
+    	String status = "ABERTO";
+    	
+    	Profissional profissional = (Profissional) request.getSession().getAttribute("profissionalLogado");
+    	
+    	String data = new SimpleDateFormat("dd/MM/yyyy").format(Calendar.getInstance().getTime());
+    	Candidatura candidatura = new Candidatura(data, status, vaga, profissional);
+    	dao.insert(candidatura);
+    	
+    	response.sendRedirect("lista");
+    	
+    	
+    }
+       
 }
